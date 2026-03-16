2b4b7d5891072fe83c45b95b9e7a8a05421d9211
Michelle Postrado <michellepostrado26@gmail.com>
2026-03-17 01:09:06 +0800

    fix database connection

--- core/database.py
+++ core/database.py
@@ -1,11 +1,21 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_db_connection():
-
-    return psycopg2.connect(
-        host="localhost",
-        dbname="demodb",
-        user="postgres",
-        password="123456789",
-        port=5432
-    )
+    database_url = os.getenv("DATABASE_URL")
+    
+    if database_url:
+        # Render PostgreSQL
+        return psycopg2.connect(database_url)
+    else:
+        # Local PostgreSQL
+        return psycopg2.connect(
+            host="localhost",
+            dbname="demodb",
+            user="postgres",
+            password="123456789",
+            port=5432
+        )
