db7dfc95e261e864a799f67a82f7c2bee6dc0694
Michelle Postrado <michellepostrado26@gmail.com>
2026-03-16 18:14:58 +0800

    changes in database connection

--- core/database.py
+++ core/database.py
@@ -1,11 +1,11 @@
 import psycopg2
+import os
 
 def get_db_connection():
-
     return psycopg2.connect(
-        host="localhost",
-        dbname="demodb",
-        user="postgres",
-        password="123456789",
-        port=5432
+        host=os.environ.get("DB_HOST"),
+        dbname=os.environ.get("DB_NAME"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        port=os.environ.get("DB_PORT", 5432)
     )
